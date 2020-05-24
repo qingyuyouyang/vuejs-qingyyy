b3626de5bf651ee5f47a920824362ffdc48caaa4
cui <2582459187@qq.com>
2020-05-24 10:53:35 +0800

    cc

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@
 * @Author: faker
 * @Date:   2020-05-24 10:42:04
 * @Last Modified by:   faker
-* @Last Modified time: 2020-05-24 10:42:10
+* @Last Modified time: 2020-05-24 10:52:58
 */
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-essential/dist/'
+    ? '/vuejs-qingyyy/dist/'
     : '/'
 }
