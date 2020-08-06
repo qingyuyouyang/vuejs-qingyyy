d70feb4a9fc15518402878816b5b588fca8f4647
cui <2582459187@qq.com>
2020-08-06 17:29:59 +0800

    product

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@
 * @Author: faker
 * @Date:   2020-05-24 10:42:04
 * @Last Modified by:   faker
-* @Last Modified time: 2020-05-24 10:52:58
+* @Last Modified time: 2020-08-06 17:29:18
 */
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-qingyyy/dist/'
+    ? '/'
     : '/'
 }
