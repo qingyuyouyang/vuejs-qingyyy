5cbaebbf670ae3ffe8423b24ad9c09b52f4d7628
cui <2582459187@qq.com>
2020-08-11 16:41:56 +0800

    process.env 线上线下环境判断

--- src/api/global_variable.js
+++ src/api/global_variable.js
@@ -2,15 +2,17 @@
 * @Author: faker
 * @Date:   2020-08-08 16:48:41
 * @Last Modified by:   faker
-* @Last Modified time: 2020-08-11 13:05:19
+* @Last Modified time: 2020-08-11 15:08:58
 */
 
-const baseURL = 'http://laravel-qingyyy.test'
+const baseURL = process.env.NODE_ENV === 'production'
+    ? 'http://laravel.qingyyy.cn'
+    : 'http://laravel-qingyyy.test'
 const token = '123456'
 const userSite = '林花落了春红，太匆匆'
 
 export default {
   baseURL,
   token,
-  userSite
+  userSite,
 }
