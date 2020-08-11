86b6e5bcb05cd66c7bec75659c8097b5d34af20b
cui <2582459187@qq.com>
2020-08-11 16:56:17 +0800

    c

--- src/api/global_variable.js
+++ src/api/global_variable.js
@@ -2,12 +2,13 @@
 * @Author: faker
 * @Date:   2020-08-08 16:48:41
 * @Last Modified by:   faker
-* @Last Modified time: 2020-08-11 15:08:58
+* @Last Modified time: 2020-08-11 16:54:59
 */
 
-const baseURL = process.env.NODE_ENV === 'production'
-    ? 'http://laravel.qingyyy.cn'
-    : 'http://laravel-qingyyy.test'
+const baseURL = process.env.NODE_ENV === 'development'
+    ? 'http://laravel-qingyyy.test'
+    : 'http://laravel.qingyyy.cn'
+console.log(process.env.NODE_ENV)
 const token = '123456'
 const userSite = '林花落了春红，太匆匆'
 
